e1e4434333d2e210f6d5ec4cfad7939ee2ebb094
LizaSmetan <krotovaliza@gmail.com>
2019-01-31 11:27:23 +0200

    new edits 31.01

--- office/js/scripts.js
+++ office/js/scripts.js
@@ -10,7 +10,9 @@ $().ready(function () {
             $(this).addClass('opened');
         }
     });
-
+    $('.header_menu nav > ul > li').on('click', function () {
+        $(this).toggleClass('opened_mobile');
+    });
     $('.header_branding__user').on('click', function () {
         $('html').toggleClass('open_user_js');
         $('html').removeClass('open_menu_js');
@@ -32,5 +34,40 @@ $().ready(function () {
     $('.pay_system').on('click', function () {
         $('.pay_system').removeClass('pay_system__chosen');
         $(this).addClass('pay_system__chosen');
-    })
+    });
+
+    $('select').select2({
+        dropdownParent: $('#token_dropdown_js')
+    });
+
+    $('#token_select_js').on('change', function (e) {
+        $('#token_value_js').text(($('#token_wallet_js').text() * $(this).val()).toFixed(2));
+    });
+    function copy(input){
+        input.focus();
+        input.setSelectionRange(0, 999999999);
+        document.execCommand('copy');
+    }
+    $('.referal_links li a').on('touchmove touchend touchstart click', function (e) {
+        var input = ($(this).prev('input'))[0];
+        var tooltip = $(this).find('.referal_links__tooltip');
+        try{
+            copy(input);
+            tooltip.text("Ссылка скопирована");
+            tooltip.addClass("_show_tooltip");
+            setTimeout(function(){
+                tooltip.removeClass("_show_tooltip");
+            }, 1500);
+        }catch(e){
+            tooltip.text("Ошибка копирования ссылки");
+            tooltip.addClass("_show_tooltip");
+            setTimeout(function(){
+                tooltip.removeClass("_show_tooltip");
+            }, 1500);
+        }
+    });
+    $('.referal_links li a').on('mouseout', function () {
+        $(this).find('.referal_links__tooltip').removeClass("_show_tooltip");
+    });
+    $('.scrollbar-inner').scrollbar();
 });
